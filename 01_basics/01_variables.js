076fef8bc1a402041f5816c715209464cc076a54
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2024-07-05 17:23:23 +0530

    learning datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = "12323"
+let accountEmail = "priyanshu4321@gmail.com"
+var accountPassword = "12345"
+accountCity = "kendrapara"
+let accountState;
+
+// acountId="14232";
+accountEmail = "hc@hc.com"
+accountPassword = "54321"
+accountCity = "cuttack"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+//prefer not to use var  because of issue in block scope and fuctional scope 
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"//treat all js code as newer version
+
+// alert(3+3)  //we are using node js not brower
+
+// console.log(3 +
+//     3
+// );//should not this write this for code readability
+
+let name = "piku"
+let age = 22
+let isLoggedIn = false
+
+//number =>2 to power 53
+//bigint 
+//string=>""
+//boolean=> true /false 
+//null=>standalone value 
+//undefined =>
+//symbol=>unique 
+
+console.log(typeof age);
+console.log(typeof null);// returns objects
+console.log(typeof isLoggedIn);
+console.log(typeof undefined);// returns undefined
