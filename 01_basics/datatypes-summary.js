3703ee96fff3e8cc36de769e200f3e72dcf4a397
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2024-07-10 02:19:48 +0530

    learning continues

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -32,6 +32,39 @@ const myFunction = function () {
     console.log("hello world");
 }
 
-console.log(typeof myFunction);//function
-console.log(typeof scoreValue);//number
-console.log(typeof heros);//object
+// console.log(typeof myFunction);//function
+// console.log(typeof scoreValue);//number
+// console.log(typeof heros);//object
+
+//more information
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//stack memory(primitive)
+//by accessing this we get a copy of the data in this type of data structure
+
+//heap memory (for non primitive data types)
+//by accessing this we get a  reference of the orignal data in this type of data structure
+
+let myname = "priyanshu"
+let anothername = myname;
+anothername = "piku";//only the copy got changed but not the original one 
+
+// console.log(myname);//Priyanshu
+// console.log(anothername);//piku
+
+let userOne={
+    email:"user@google.com",
+     upi:"user@ubl"
+}
+
+let userTwo=userOne;
+userTwo.email="piku@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email); //both will return piku@gmail.com
+//as the reference of both the data are same and both got changed
+
+
