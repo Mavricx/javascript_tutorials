2e679025788dd384716b71cdff4500dbe63f427a
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2024-07-24 01:26:57 +0530

    learning about function and objects in javascript

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -31,14 +31,57 @@ function addTwoNumbers(number1, number2) {
 const result = addTwoNumbers(33, 34);
 console.log(result);//67
 
-function loginUserMessage(username="sam"){//if nothing is passed in the username then sam will be passed as default
-    if(username==undefined){ //if(!username) is also valid
+function loginUserMessage(username = "sam") {//if nothing is passed in the username then sam will be passed as default
+    if (username == undefined) { //if(!username) is also valid
         console.log("please enter a username");
         return;
     }
-    
+
     return `${username} just logged in`;
 }
 
 console.log(loginUserMessage("priyanshu"));//priyanshu just logged in
 console.log(loginUserMessage());//undefined just logged in
+
+function calculateCartPrice(...num1) {//rest operator/spread operator -we can pass multiple arguments
+    return num1;
+}
+
+console.log(calculateCartPrice(200, 300, 400));//[ 200, 300, 400 ]  //returns an array
+
+//another example of spread/rest operator
+
+function spreadOperatorExample(val1, val2, ...num1) {
+    return num1;
+}
+
+console.log(spreadOperatorExample(200, 300, 400, 1200, 42334));//[ 400, 1200, 42334 ]
+
+//passing objects in a function
+
+const user = {
+    username: "piku",
+    price: 199
+}
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+handleObject(user);//Username is piku and price is 199
+handleObject({//passing object itself in the function
+    username: "priyanshu",
+    price: 399,
+})//Username is priyanshu and price is 399
+
+//----passing array in a function
+
+const arr = [200, 300, 400];
+function returnSecondValue(getArray) {
+    return getArray[1];
+}
+
+console.log(returnSecondValue([10,20,30]));//20
+
+
+
