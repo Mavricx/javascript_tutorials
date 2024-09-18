48ee88b9281906339b7b529a77cde4bff69f9fdd
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2024-09-19 02:41:53 +0530

    finished learning about basic sql qureies

--- 17_sql/insta.sql
+++ 17_sql/insta.sql
@@ -31,3 +31,64 @@ VALUES
 
 select id , name ,email from user;
 select distinct age from user;
+
+select name ,age ,followers from user
+where age >15 and followers>200;
+ 
+select name ,age ,followers from user
+where age between 20 and 30;
+
+select name , age , followers from user
+where email in ("david@example.com","eve@example.com"); 
+
+select name , age , followers from user
+where age in (22,25);
+
+select name ,age email  from user
+where age not in (20,30);
+
+select name, age, followers from user order by followers ASC;
+
+select max(followers) from user;
+select count(age) from user where age>14; 
+
+select age,max(followers) 
+         from user 
+         where age >24
+         group by age 
+         having  max(followers) >200
+         order by age desc;
+
+
+select age, max(followers) from user  group by age;
+
+select age,max(followers) 
+from user 
+where age >24
+group by age 
+having  max(followers) >200
+order by age desc;
+
+
+
+update user 
+set followers=600
+where age=22;
+
+
+delete from user where age =35;
+
+SET SQL_SAFE_UPDATEs=0;
+select * from user;
+
+alter table user add column city varchar (34) default "delhi";
+alter table user drop column age;
+alter table user rename to instauser;
+alter table instauser rename to user;
+alter table user modify subs int default 5;
+
+INSERT INTO user (id, name, email, following) 
+VALUES 
+(9, 'Alice', 'pentagon@example.com', 200);
+drop table post;
+truncate table user;
