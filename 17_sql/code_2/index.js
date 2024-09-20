5f1792a1afbe5bb1b1d349103dddf42d98bf58ee
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2024-09-21 02:27:59 +0530

    trying out new functions for data insertion

--- 17_sql/code_2/index.js
+++ 17_sql/code_2/index.js
@@ -4,11 +4,11 @@ const mysql = require('mysql2');
 const generateData = () => {
     return [
         faker.string.uuid(),
-        faker.internet.email(),
         faker.internet.userName(),
+        faker.internet.email(),
         faker.internet.password()
-    ]
-};
+    ];
+}
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -18,21 +18,37 @@ const connection = mysql.createConnection({
     password: 'Pikulhavesql@11'
 });
 
-
-
-const commandQuery = (q) => {
-    try {
-        connection.query(q, (err, result) => {
-            if (err) throw err;
-            console.log(result);
-        });
-    }
-    catch (err) {
-        console.log(err)
-    }
+let data = [];
+for (let i = 0; i < 10; i++) {
+    data.push(generateData());
 }
+// Function to execute a command query
+const commandQuery = (q) => {
+    connection.query(q, [data],(err, result) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            return;
+        }
+        console.log(result);
+    });
+};
 
-commandQuery("show databases");
-commandQuery("use delta_app");
-commandQuery("show tables");
+// Function to execute a normal query string
+const normalQuery = (q) => {
+    connection.query(q, (err, result) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            return;
+        }
+        console.log(result);
+    });
+};
+
+
+// commandQuery("use delta_app");
+// commandQuery("create table persons (id int primary key ,username varchar(70) unique,email varchar(70) unique ,password varchar(255) )");
+// commandQuery("insert into persons (id,username,email,password) values ?",);
+normalQuery("select * from persons")
+// console.log(data);
+// normalQuery("create table persons (id varchar(70) primary key ,username varchar(70) unique,email varchar(70) unique ,password varchar(255) )");
 connection.end();
